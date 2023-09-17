2c44f24bab9534a4945b5bb799691631080bb065
Will Barnes <will.t.barnes@gmail.com>
2023-09-16 20:12:36 -0400

    use a0 portrait; figsize in rc file

--- scripts/plot_figure.py
+++ scripts/plot_figure.py
@@ -13,14 +13,7 @@ angle = np.linspace(-2*np.pi, 2*np.pi, 1000)
 y_1 = np.cos(angle)
 y_2 = np.sin(angle)
 
-# This is the width of a column in the poster
-# as defined by the \colwidth variable. Adjust this
-# depending on the column width you use and then
-# scale all figure sizes to this variable
-column_width = 13.5028346256
-golden_ratio = (1 + np.sqrt(5))/2
-
-fig = plt.figure(figsize=(0.65*column_width, 0.65*column_width/golden_ratio), layout='constrained')
+fig = plt.figure(layout='constrained')
 ax = fig.add_subplot()
 ax.plot(angle, y_1, label=r'$\cos{(x)}$')
 ax.plot(angle, y_2, label=r'$\sin{(x)}$')
